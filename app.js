bb05107bb9c1e177db1279e875b63b75a86bb62b
Arbenz-007 <rayyan828113@gmail.com>
2025-01-13 22:58:12 +0530

    ep3 with comments

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-import React  from "react";
+import React from "react";
 import * as ReactDOM from "react-dom/client";
+import { jsx } from "react/jsx-runtime";
 
+//React.createElement =>React element--object=>becomes HTML element after we render it
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "i'm a h1 tag"),
-        React.createElement("h2", {}, "I'm a h2 tag 👌")//array of children for creating siblings
-        ]),
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "i'm a h1 tag 👍"),
-        React.createElement("h2", {}, "I'm a h2 tag")//array of children for creating siblings
-        ])
-]);
+//JSX -- is not HTML in JS its diff
+//    -- it looks like HTML or XML syntax
+//    -- JSX is a syntax extension for JavaScript not a valid pure JS
+//JSX(transpiled before it reaches the JS )--and Transpiling is done by Parcel -- and parcel gives this task to package -Babel.
+//behind the scenes JSX code => React.createElement=>Reactelement- JS object=> HTML(render)
 
-console.log(parent);
-const root =ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent);
+const heading = (
+  <h1 className="head" tabIndex="1">
+    Learning React using JSX
+  </h1>
+);
 
-//JSX  
+//React component
+//class based components----OLD
+//function based components-NEW
+
+//React functional Component--> a JS func which return a some JSX
+
+// const HeadingComponent = () => {
+//   return <h1>Learning React Component</h1>;
+// };
+
+// const HeadingComponent2 = () => <h1>Learning React Component</h1>;
+
+const HeadingComponent3 = () => (
+  <div id="container">
+    <h1 className="heading">Learning React Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);//this is how we render a react element
+
+root.render(<HeadingComponent3/>);//this is how we render a REACT COMPONENT
