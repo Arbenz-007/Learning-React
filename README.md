49397c4222af5ccdd2d56e6471db9ea42e45778c
Arbenz-007 <rayyan828113@gmail.com>
2025-01-13 17:19:57 +0530

    ep2

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# learning React
+# learning React 🙌
+
+# Parcel
+-Dev Build
+-Local Server
+-HMR=Hot Module Replacement
+-File watching algo -- written in c++
+-Caching - Faster Builds
+-Image optimisation
+-Bundling
+-Compressing
+-Consistent Hashing
+-Differential Bundling -- support older browsers
+-Diagonastic
+-Error Handling
+-HTTPs
+-Tree Shaking --remove unused codes
+-Different dev and prod bundles
+
+
+
+
+ 
--- app.js
+++ app.js
@@ -1,38 +1,20 @@
-const heading = React.createElement("h1",    //heading:its an js object of type h1
-    { id: "heading", xyz: "abc" },
-    "Hello world from react insisde it");
-console.log(heading);//object
+import React  from "react";
+import * as ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);//responsible for converting object to h1 tag and putting it in root
-/*
-<div id="parent">
-    <div id="child">
-        <h1>I,m h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I,m h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div>
-
-ReactElement(object)->HTML(Browser Understands)
-*/
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" },
         [React.createElement("h1", {}, "i'm a h1 tag"),
-        React.createElement("h2", {}, "I'm a h2 tag")//array of children for creating siblings
+        React.createElement("h2", {}, "I'm a h2 tag 👌")//array of children for creating siblings
         ]),
     React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "i'm a h1 tag"),
+        [React.createElement("h1", {}, "i'm a h1 tag 👍"),
         React.createElement("h2", {}, "I'm a h2 tag")//array of children for creating siblings
         ])
 ]);
 
 console.log(parent);
+const root =ReactDOM.createRoot(document.getElementById("root"))
 root.render(parent);
 
 //JSX  
