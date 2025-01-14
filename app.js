0210c5704008bb61d532d88425e1e3057b78384c
Arbenz-007 <rayyan828113@gmail.com>
2025-01-14 20:48:12 +0530

    ep3

--- app.js
+++ app.js
@@ -2,40 +2,37 @@ import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { jsx } from "react/jsx-runtime";
 
-//React.createElement =>React element--object=>becomes HTML element after we render it
-
-//JSX -- is not HTML in JS its diff
-//    -- it looks like HTML or XML syntax
-//    -- JSX is a syntax extension for JavaScript not a valid pure JS
-//JSX(transpiled before it reaches the JS )--and Transpiling is done by Parcel -- and parcel gives this task to package -Babel.
-//behind the scenes JSX code => React.createElement=>Reactelement- JS object=> HTML(render)
-
-const heading = (
+const Title = () => (
   <h1 className="head" tabIndex="1">
     Learning React using JSX
   </h1>
 );
 
-//React component
-//class based components----OLD
-//function based components-NEW
+const ele2=<span>React Element</span>;
 
-//React functional Component--> a JS func which return a some JSX
+const ele = (
+  <h1 className="head" tabIndex="5">
+    {ele2}
+    Putting element inside a component
+  </h1>
+);
 
-// const HeadingComponent = () => {
-//   return <h1>Learning React Component</h1>;
-// };
 
-// const HeadingComponent2 = () => <h1>Learning React Component</h1>;
 
+const number=10000;
+
+//componmen Composition -- a component within a component
+// inside a component within a {} we can execute any javascript line
 const HeadingComponent3 = () => (
   <div id="container">
+    {Title()}
+    <Title></Title>
+    <Title />
+    {ele} 
     <h1 className="heading">Learning React Component</h1>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);//this is how we render a react element
-
-root.render(<HeadingComponent3/>);//this is how we render a REACT COMPONENT
+root.render(<HeadingComponent3 />); //this is how we render a REACT COMPONENT
